905baf478ab6f9446e71dc165510f5c334fba0b8
Paddy McGlone <paddymcglone@gmail.com>
2019-09-10 21:08:11 +0100

    Adding the new controller and view

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ActionResult Random()
+        {
+            return View(new Movie{Name = "The Italian Job"});
+        }
+    }
+}
