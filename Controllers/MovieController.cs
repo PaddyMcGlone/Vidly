d2a062879568cc08874d854fead427d486e8870b
Paddy McGlone <paddymcglone@gmail.com>
2019-09-15 10:02:44 +0100

    Update MovieController.cs
    - Adding new custom route to action.

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -14,6 +14,7 @@ namespace Vidly.Controllers
             return View(new Movie{Name = "The Italian Job"});
         }
 
+        [Route("Movies/Released/{year}/{month}")]
         public ActionResult Released(int year, int month)
         {
             return Content($"Year {year} & Month {month} - Yes Paddy !");
