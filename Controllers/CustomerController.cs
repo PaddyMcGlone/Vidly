53b7ceb01f38f25e199b47330687412361b68f40
Paddy McGlone <paddymcglone@gmail.com>
2019-09-16 19:21:20 +0100

    Adding the customer update

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private List<Customer> Customers { get; }
+
+        public CustomerController()
+        {
+            Customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Richard Hobson"},
+                new Customer {Id = 2, Name = "Catherine Scott"},
+                new Customer {Id = 3, Name = "Tim Craig"}
+            };
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        public ActionResult ViewCustomer(int Id)
+        {
+            var customer = Customers.Find(c => c.Id == Id);
+            return View(customer);
+        }
+    }
+}
